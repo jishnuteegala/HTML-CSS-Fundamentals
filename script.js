afda578d2a8916c1f5c0f65cbfb40f0cb32a0444
Jishnu Teegala <134275562+jishnuteegala@users.noreply.github.com>
2024-10-04 16:24:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const inventory = {
+    apple: { price: 12, qty: 0 },
+    cherry: { price: 15, qty: 0 },
+    strawberry: { price: 18, qty: 0 }
+}
+
+function sum() {
+    let total = 0;
+
+    const keys = Object.keys(inventory);
+    keys.forEach(key => {
+        total += inventory[key].price * inventory[key].qty;
+    })
+
+    return total;
+}
+
+function calculate(box) {
+    let qty = 0;
+
+    if (box.value.length > 0) {
+        qty = parseInt(box.value);
+    }
+
+    inventory[box.id].qty = qty;
+    const total = sum();
+
+    return `$${total}.00`;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const totalContainer = document.getElementById('total-container');
+
+    const inputBoxes = document.querySelectorAll('#calculator input');
+
+    inputBoxes.forEach(box => {
+
+        box.addEventListener('change', () => {
+            totalContainer.textContent = calculate(box);
+        });
+
+        box.addEventListener('keyup', () => {
+            totalContainer.textContent = calculate(box);
+        });
+
+    });
+});
